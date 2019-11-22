e4c711e1a01aaa6561d8a799a7cc3cad6a70605b
Dariana Green <darianapro@gmail.com>
2019-11-22 18:08:14 +0100

    not working commit

--- Backend/app.js
+++ Backend/app.js
@@ -6,7 +6,7 @@ var session = require('express-session');
 var cors = require('cors');
 var MongoDB = require('./models/user').Mongo;
 
-MongoDB.connect('mongodb://localhost:27017/database', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+MongoDB.connect('mongodb://localhost:27017/cinemaCity', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
     console.log('MongoDB is connected')
 }).catch(error => {
     console.log('MongoDB is NOT connected.' + error)
--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,19 +1,34 @@
 import React from 'react';
 
 import './App.css';
+class loginBackendResponse extends React.Component {
 
-function App() {
-  return (
-   <main> 
-    <form id="loginForm">
-      <input id="loginName" type="text" placeholder="insert your name"></input>
-      <input id="loginPassword" type="password" placeholder="insert your password"></input>
-    <button id="submitButton" type="submit">LOGIN</button>
-
-    </form>
-
-    </main>
-  );
+constructor(props) {
+super(props);
+this.state = {username: '', password: ''}
 }
 
-export default App;
+async login() {
+const result = await fetch('http://localhost:9000/users/login', {
+  method: 'post',
+  body: {
+   username: "darianapro",
+   password: "1234567"
+  }
+ })
+ alert(JSON.stringify(result))
+}
+  render() {
+    return(<main> 
+      <form id="loginForm">
+        <input id="loginName" type="text" placeholder={this.state.username}></input>
+        <input id="loginPassword" type="password" placeholder="insert your password"></input>
+      <button id="submitButton" type="submit" onClick={this.login}>LOGIN</button>
+  
+      </form>
+  
+      </main>);
+  }
+}
+
+export default loginBackendResponse;
